f0a6f5b1be2cb55ccaa92140878eacc19d64afb6
EdG570 <cole570@hotmail.com>
2017-03-18 10:31:57 -0400

    Added TagDb to data access layer

---
+++ DAL/TagDb.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BOL;
+
+namespace DAL
+{
+    public class TagDb
+    {
+        private BlogEntities db;
+
+        public TagDb()
+        {
+            db = new BlogEntities();
+        }
+
+        public IEnumerable<Tag> GetAll()
+        {
+            return db.Tags.ToList();
+        }
+
+        public Tag GetById(int id)
+        {
+            return db.Tags.Find(id);
+        }
+
+        public void Insert(Tag tag)
+        {
+            db.Tags.Add(tag);
+            Save();
+        }
+
+        public void Delete(int id)
+        {
+            Tag tag = GetById(id);
+            db.Tags.Remove(tag);
+            Save();
+        }
+
+        public void Update(Tag tag)
+        {
+            db.Entry(tag).State = EntityState.Modified;
+            Save();
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+    }
+}
